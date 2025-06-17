6e5ce8b85edd0c364fdec257707d22605b67557c
Ravi Upadhyay <93428584+RaviUpadhyay1@users.noreply.github.com>
2025-06-17 16:43:10 +0545

    commit

---
+++ Day 12/script.js
@@ -0,0 +1,41 @@
+let arr = [1,2,3,4,5]
+console.log(arr);
+console.log(arr.length);
+console.log(arr[0])
+arr[4]= 12;
+console.log(arr)
+console.log(typeof(arr))
+
+console.log(arr.toString())
+console.log(arr)
+console.log(arr.join(" and "))
+console.log(arr.pop())
+console.log(arr)
+console.log(arr.push(5))
+console.log(arr)
+console.log(arr.shift())
+console.log(arr)
+console.log(arr.unshift(1))
+console.log(arr)
+console.log(delete arr[4])
+console.log(arr)
+console.log(arr.length)
+
+let a = [1,2,3]
+let b = [4,5,6]
+console.log(a.concat(b))
+
+let c = [2,4,1,3]
+console.log(c.sort())
+
+let d = [1,2,3,4,5]
+console.log(d.splice(1,2))
+console.log(d)
+
+let e = [1,2,3,4,5,6]
+console.log(e.slice(2))
+console.log(e.slice(1,3))
+console.log(e)
+
+let rev =[1,2,3,4,5,6]
+console.log(rev.reverse())
