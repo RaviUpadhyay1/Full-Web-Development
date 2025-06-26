073929b700e95c56723224584b94596bc5c2805c
Ravi Upadhyay <93428584+RaviUpadhyay1@users.noreply.github.com>
2025-06-26 21:31:39 +0545

    Update class&objects.js
    commit

--- Day 17/class&objects.js
+++ Day 17/class&objects.js
@@ -1,5 +1,15 @@
 class Animal{
-    constructor(){
+    constructor(name){
+        this.name = name
         console.log("Object is created..")
     }
+
+    eats(){
+        console.log("Kha raha hoon")
+    }
+    jumps(){
+        console.log("kood raha hu")
+    }
 }
+let a = new Animal("Bunny");
+console.log(a)
