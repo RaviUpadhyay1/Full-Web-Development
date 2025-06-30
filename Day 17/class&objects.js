a3ae8244f2b18a88d07535bc3047de43d2d355c7
Ravi Upadhyay <93428584+RaviUpadhyay1@users.noreply.github.com>
2025-06-30 21:13:09 +0545

    commit

--- Day 17/class&objects.js
+++ Day 17/class&objects.js
@@ -11,5 +11,26 @@ class Animal{
         console.log("kood raha hu")
     }
 }
+// extends Allows a class to inherit from another.
+class Lion extends Animal{
+
+    constructor(name){
+        // super() Calls the constructor of the parent class.
+        super(name) // calls the parent (Animal) constructor
+        console.log("Object is created and he is a lion..")
+    }
+    // Method Overriding -A child class defines its own version of a parent class method.
+    eats(){
+        // super.methods() Calls a method from the parent class.
+        super.eats() // calls the parent's eats()
+        console.log("kha raha hoon mein")
+    }
+
+
+}
 let a = new Animal("Bunny");
 console.log(a)
+
+
+let l = new Lion("Shera")
+console.log(l)
